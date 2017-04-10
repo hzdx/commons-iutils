af9d7ddc97ae50c282c336b4c6e308e546d7c89c
ldm7777 <hzdx102468@163.com>
2017-04-10 18:50:48 +0800

    22

--- commons-utility/src/main/java/test/GitTest.java
+++ commons-utility/src/main/java/test/GitTest.java
@@ -9,7 +9,6 @@ public class GitTest {
 		for(int i = 0 ; i < 3 ; i++){
 			System.out.println("test....................");
 		}
-		System.out.println("2222222222222222222222");
 		
 		System.out.println("third line");
 		
