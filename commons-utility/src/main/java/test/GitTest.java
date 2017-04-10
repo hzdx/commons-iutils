3f9618475bf5aef5c4fe052354d1be3865bc1aeb
ldm7777 <hzdx102468@163.com>
2017-04-10 16:47:13 +0800

    revert

--- commons-utility/src/main/java/test/GitTest.java
+++ commons-utility/src/main/java/test/GitTest.java
@@ -3,14 +3,16 @@ package test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("firt line");
 		System.out.println("send line");
-		
+		System.out.println("11111111111");
 		for(int i = 0 ; i < 3 ; i++){
 			System.out.println("test....................");
 		}
+		System.out.println("2222222222222222222222");
 		
 		System.out.println("third line");
+		
+		System.out.println("3333333333333333333333");
 
 	}
 
