faad7fd6cb1117f2bdb77e6687f39f6a31a715f9
ldm7777 <hzdx102468@163.com>
2017-04-11 10:39:38 +0800

    bbb

--- commons-utility/src/main/java/test/GitTest.java
+++ commons-utility/src/main/java/test/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello all!");
+		System.out.println("hello bbb!");
 		System.out.println("send line");
 	
 		
